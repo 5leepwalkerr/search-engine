84a1452cfb6687c776bbd8bbf6f175721087cf32
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-03-07 16:36:59 +0300

    init commit

---
+++ src/main/java/com/skillbox/project/SkillboxPrjctApplication.java
@@ -0,0 +1,13 @@
+package com.skillbox.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillboxPrjctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillboxPrjctApplication.class, args);
+    }
+
+}
